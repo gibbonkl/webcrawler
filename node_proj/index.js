1d66b979242cd0d3c8dd6ab43e16340fdfedc92d
gibbonkl <gibbon@furg.br>
2021-03-02 21:38:13 -0300

    limpeza do código

--- node_proj/index.js
+++ node_proj/index.js
@@ -2,7 +2,6 @@ const puppeteer = require("puppeteer");
 
 (async () => {
   const browser = await puppeteer.launch();
-
   const page = await browser.newPage();
 
   let inicialPage = "https://ifrs.edu.br/riogrande";
@@ -16,38 +15,27 @@ const puppeteer = require("puppeteer");
   let crawledUrls = [];
   let index = -1;
 
-  // crawling function to gather urls
   async function getUrls(urlIn) {
     await page.goto(urlIn);
 
-    // gather all urls of the page
     crawledUrls = await page.$$eval("a", (assetLinks) =>
       assetLinks.map((link) => link.href)
     );
 
-    // selecting urls of interest
     for (let crawledUrl of crawledUrls) {
-      // only gets new matching pages
       if (reg.test(crawledUrl) && selectedUrls.indexOf(crawledUrl) < 0)
         selectedUrls.push(crawledUrl);
     }
 
-    // interaction increment
     index++;
   }
 
   await getUrls(inicialPage);
 
-  try {
-    while (selectedUrls.length > index && index < limitCrawledPages) {
-      await getUrls(selectedUrls[index]);
-    }
-  } catch (error) {
-    print(error);
+  while (selectedUrls.length > index && index < limitCrawledPages) {
+    await getUrls(selectedUrls[index]);
   }
 
-  console.log(selectedUrls);
-  console.log("Pages crawled: " + index);
   console.log("Pages selected: " + selectedUrls.length);
 
   browser.close();
